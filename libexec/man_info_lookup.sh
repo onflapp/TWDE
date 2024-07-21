eb06e0a254b72fcc4d278bfddf6a17b58c3261aa
onflapp <onflapp@gmail.com>
2024-07-21 13:02:36 +0200

    enhancements

--- libexec/man_info_lookup.sh
+++ libexec/man_info_lookup.sh
@@ -2,9 +2,14 @@
 
 WORD="$1"
 
+. $TWDE_HOME/etc/manual.conf
+
 while [ -z "$WORD" ];do
   echo "manpage for? "
   read WORD
 done
+
+export MANWIDTH="$(($WIDTH - 2))"
+export MANPAGER="$PAGER"
 
 man "$WORD"
