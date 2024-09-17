a72108c06b562268e21874bf1b8a4e3af26957b2
onflapp <onflapp@gmail.com>
2024-09-17 14:53:54 +0100

    improvements

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 cd src
 make install
+
 chown root:root $TWDE_HOME/libexec/gesture_helper
 chmod 4775 $TWDE_HOME/libexec/gesture_helper
+
+chown root:root $TWDE_HOME/libexec/keyboard_helper
+chmod 4775 $TWDE_HOME/libexec/keyboard_helper
